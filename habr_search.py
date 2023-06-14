17d13e928445b55ecb5fec4cb144d7d2e110738b
chmutenkoa <alenachm@gmail.com>
2023-06-14 12:50:05 +0300

    get_full_text() added

--- habr_search.py
+++ habr_search.py
@@ -1,34 +1,61 @@
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
-
-URL = 'https://habr.com/ru/search/'
-queries = ['python', 'анализ данных']
+import time
 
 
-def search_on_habr(queries):
+def search_on_habr(queries, pages):
+    url = 'https://habr.com/ru/search/'
+
+    def parse_article(article):
+        date = (
+            pd.to_datetime(
+                article.find('time').get('datetime')
+            ).date()
+        )
+        title = article.find('h2').text
+        if not article.find('a', 'tm-title__link'):
+            link = article.find('a').get('href')
+        else:
+            link = article.find('a', 'tm-title__link').get('href')
+        full_link = f'https://habr.com{link}'
+        likes = article.find('span', 'tm-votes-meter__value').text
+        return {'date': date, 'title': title, 'link': full_link, 'likes': likes}
+
     result = pd.DataFrame()
+
     for query in queries:
-        params = {'q': query}
-        response = requests.get(URL, params).text  # GET-request for the query from the 'queries' list
-        soup = BeautifulSoup(response, features='lxml')
-        articles = soup.findAll('div', 'tm-article-snippet')
-        for article in articles:
-            date = (
-                pd.to_datetime(
-                    article.find('span', 'tm-article-datetime-published')
-                    .find('time').get('datetime')
-                ).date()
-            )
-            title = article.find('h2', 'tm-title tm-title_h2').find('span').text
-            link = article.find('h2', 'tm-title tm-title_h2').find('a').get('href')
-            full_link = f'habr.com{link}'
-            row = {'date': date, 'title': title, 'link': full_link}
-            result = pd.concat([result, pd.DataFrame([row])], ignore_index=True)
-    result.drop_duplicates(subset=['link'], inplace=True)
+        params = {'q': query}  # parameter for GET request
+
+        for page in range(1, pages + 1):
+            url_with_page = f'{url}page{page}/'
+            response = requests.get(url_with_page, params).text  # GET-request for the query from the 'queries' list
+            time.sleep(0.3)
+            soup = BeautifulSoup(response, features='lxml')
+            articles = soup.find_all('article', 'tm-articles-list__item')
+
+            for article in articles:
+                row = parse_article(article)  # the dict as a row for result DataFrame
+                result = pd.concat([result, pd.DataFrame([row])], ignore_index=True)
+
+    result.drop_duplicates(subset=['link'], inplace=True, ignore_index=True)
     return result
 
 
-print(search_on_habr(queries))
+def get_full_text(posts_df):
+    for i, link in enumerate(posts_df['link']):
+        req = requests.get(link).text
+        soup = BeautifulSoup(req, features='lxml')
+        if soup.find('div', 'tm-article-body'):
+            full_text = soup.find('div', 'tm-article-body').text.strip()
+            posts_df.loc[i, 'text'] = full_text
+    return posts_df
 
 
+def main():
+    queries = ['python', 'анализ данных']
+    posts = search_on_habr(queries, 3)
+    print(get_full_text(posts)[['title', 'text']].head())
+
+
+main()
